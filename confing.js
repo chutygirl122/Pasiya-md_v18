15704aa7a6e84fcebfe0591a4ae3deedaed53007
chutygirl122 <jokamalli191@gmail.com>
2025-06-28 13:13:03 +0530

    Create confing.js

---
+++ confing.js
@@ -0,0 +1,6 @@
+global.botNumber = "+94784548818"; // change this with your number, start with your country code number
+global.bot = {
+  antiCall: true, // auto reject call switch
+  owner: ["+94784548818"], // owner number
+  sessionName: "PASIYA-MD_session", // PASIYA-MD_seson
+}
